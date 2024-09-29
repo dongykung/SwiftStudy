1effaf040aa0cc0d9a2e67bafe82c748a9c6958e
kim dong kyung <kdkd422@naver.com>
2024-09-30 06:46:44 +0900

    Add iOS17+ URLSession 모델

--- URLSession/URLSession/URLSession/iOS17Paging/Photo.swift
+++ URLSession/URLSession/URLSession/iOS17Paging/Photo.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Photo: Identifiable, Codable, Hashable {
+    var id: String
+    var author: String
+    var url: URL
+    var downloadURLString: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case author
+        case url
+        case downloadURLString = "download_url"
+    }
+    
+    var downloadloadURL: URL? {
+        URL(string: downloadURLString)
+    }
+    
+    var imageURL: URL? {
+        URL(string: "https://picsum.photos/id/\(id)/256/256.jpg")
+    }
+}
+
