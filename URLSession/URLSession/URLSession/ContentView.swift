914ceba15e231d6abd6375ffbc1ea1bbd25ddfa7
kim dong kyung <kdkd422@naver.com>
2024-09-30 03:44:43 +0900

    urlSession

---
+++ URLSession/URLSession/URLSession/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSession
+//
+//  Created by 김동경 on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSession/URLSession/URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by 김동경 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
